c36e7d5af102d7dc62b5343ab06fba293314295f
Mark Weishaar <mweishaar@gmail.com>
2025-09-20 06:32:13 -0500

    improved logging - fixed to run in cron

--- ts2mp4.sh
+++ ts2mp4.sh
@@ -2,30 +2,38 @@
 # Script to convert OTA captured .ts files to .mp4
 # Uses: https://hub.docker.com/r/jlesage/handbrake
 
+log=/var/log/$(basename $0 |cut -f1 -d'.').log
+
+logmessage() {
+  timestamp=$(date '+%b %d %T')
+  echo $timestamp "$1" >> $log
+}
+
 # Check if transcoder is currently running
 proctest=$(ps -ef |grep Transcoder |grep -v grep)
 if [ -n "$proctest" ]
 then
-  echo "Transcoder currently running - exiting"
+  logmessage "Transcoder currently running - exiting"
   exit 1
 fi
 
 # Check if there is any work to do
-if [ -z $(find /dvr/ /plex/tv/ -name "*.ts" -type f) ]; then
-  echo "No transport stream files to process"
+if [ -z "$(find /dvr/ /plex/tv/ -name "*.ts" -type f)" ]; then
+  logmessage "No transport stream files to process"
   exit 0
 fi
 
-# Bring up docker container
-docker compose -f /docker/handbrake/docker-compose.yml up -d
-
+# Convert the files
 for file in $(find /dvr/ /plex/tv/ -name "*.ts" -type f |tr " " _)
 do
+  # Start Handbrake container
+  docker compose -f /docker/handbrake/docker-compose.yml up -d
   file2convert=$(echo "${file}" |tr _ " ")
   destfilename="${file2convert%.ts}.mp4"
-  echo "Converting ${file2convert}"
-  docker exec -it handbrake-handbrake-1 HandBrakeCLI -i "/storage/${file2convert}" -o "/storage/${destfilename}" --preset="Devices/Apple 1080p30 Surround" --encoder-preset="veryfast" -O && rm -f "${file2convert}" || echo "Error converting ${file2convert}"
+  logmessage "Converting ${file2convert}"
+  docker exec -i handbrake-handbrake-1 HandBrakeCLI -i "/storage/${file2convert}" -o "/storage/${destfilename}" --preset="Devices/Apple 1080p30 Surround" --encoder-preset="veryfast" -O && rm -f "${file2convert}" || echo "Error converting ${file2convert}"
+  logmessage "Status of ${file2convert} conversion = $?"
+  chown plex:plex "${destfilename}"
+  # Stop Handbrake container
+  docker compose -f /docker/handbrake/docker-compose.yml stop
 done
-
-# Shut down docker container
-docker compose -f /docker/handbrake/docker-compose.yml down
